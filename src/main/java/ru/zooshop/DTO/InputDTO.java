ccd5b3a956aa17d0849edda2a5b974e355832e47
aleshard <mikhai.alex>
2022-02-06 17:50:43 +0300

    Get and Post methods

---
+++ src/main/java/ru/zooshop/DTO/InputDTO.java
@@ -0,0 +1,9 @@
+package ru.zooshop.DTO;
+
+import lombok.Data;
+
+@Data
+public class InputDTO {
+    private String id;
+    private String value;
+}
--- src/main/java/ru/zooshop/conrollers/MainController.java
+++ src/main/java/ru/zooshop/conrollers/MainController.java
@@ -1,14 +1,49 @@
 package ru.zooshop.conrollers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.zooshop.DTO.InputDTO;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/")
 public class MainController {
+    private ArrayList<HashMap<String, String>> animals = new ArrayList<>() {{
+        add(new HashMap<>() {{
+            put("4","Animal1");
+            put("5","Animal2");
+            put("6","Animal3");
+        }});
+    }};
+
     @GetMapping
-    public String getTest( ) {
-        return "it's start";
+    public ArrayList<HashMap<String, String>> getTest( ) {
+        return animals;
+    }
+
+    @GetMapping(value = "/{id}")
+    public String getElement(@PathVariable  String id,
+                             HttpServletResponse response) throws IOException {
+        for (var map :
+                animals) {
+            var set = map.keySet();
+            if (map.containsKey(id)) {
+                return map.get(id);
+            }
+        }
+        response.sendError(1);
+        return "null";
+    }
+
+    @PostMapping(value = "/post")
+    public void getElement(HttpServletResponse response,
+                           @RequestBody InputDTO mapa) throws IOException {
+        animals.add(new HashMap<>() {{
+            put(mapa.getId(), mapa.getValue());
+        }});
+        response.sendRedirect("/");
     }
 }
